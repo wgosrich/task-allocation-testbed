b99276763e50132a20d6da3d755ad6a89a04c9d2
Walker Gosrich <wgosrich@gmail.com>
2020-06-01 15:25:23 -0400

    added simple planner that just returns identity assignment

--- simple-env.py
+++ simple-env.py
@@ -46,3 +46,4 @@ class SimpleEnv():
                     self.done[task] = True
 
 
+
---
+++ simple-planner.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+class SimplePlanner():
+
+
+    def __init__(self):
+        pass
+
+    def plan(self,agent_state,task_state):
+        n_agents = agent_state.shape[0]
+        n_tasks = task_state.shape[0]
+        assert n_agents == n_tasks
+        return np.eye(n_tasks)
