038c61cf41e2f9cb6354869dc05cddc010ac6b06
Walker Gosrich <wgosrich@gmail.com>
2020-06-01 14:08:48 -0400

    added check_progress function that tracks which tasks get completed

--- simple-env.py
+++ simple-env.py
@@ -9,21 +9,40 @@ class SimpleEnv():
 
     def __init__(self):
         self.dt = 0.1
+        self.eps = 0.1
         self.n_agents = 3
-        self.n_goals = 3
+        self.n_tasks = 3
 
         #initialize robot state
         self.x = np.random.rand(n_agents,2)
 
         #choose random goals
-        self.goals = np.random.rand(n_goals,2)
+        self.tasks = np.random.rand(n_tasks, 2)
+
+        self.assignment_matrix = np.zeros((self.n_agents,self.n_tasks),dtype=bool)
+        
+        self.done = np.zeros((n_agents,),dtype=bool)
 
     def step(self, action):
         """
         :param action: n_agents x 2 numpy array
-        :return: new state x
+        :return: new state x, completion states of all tasks
         """
         #TODO: check if action is valid
 
         self.x = self.x + self.dt*action
-        return self.x
+
+        self.check_progress()
+        return self.x, self.done
+
+    def check_progress(self):
+        for robot in range(self.n_agents):
+            inds = np.arange(self.n_tasks)
+            assigned_tasks = inds[self.assignment_matrix[robot,:]]
+            for task in assigned_tasks:
+                loc = self.tasks[task,:]
+                dist = np.linalg.norm(loc-self.x[robot,:])
+                if dist<self.eps
+                    self.done[task] = True
+
+
