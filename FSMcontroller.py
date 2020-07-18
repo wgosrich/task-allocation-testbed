e7f95b82c2dd0230a9e495079ac683ed71d5a06b
Tuo <willwong1016@gmail.com>
2020-07-18 11:07:30 -0400

    modified

--- FSMcontroller.py
+++ FSMcontroller.py
@@ -1,16 +1,59 @@
 from statemachine import StateMachine, State
+import numpy as np
+
+
 
 class collisionAvoidance(StateMachine):
     start = State('Start', initial=True)
     straight = State('Straight')
     circle = State('Circle')
     finish = State('Finish')
-    # the lines above describe three possible state robots have: start -> straight, straight -> finish, straight -> circle, circle -> ctraight
+    # the lines above describe three possible state robots have: start -> straight, straight -> finish, straight ->
+    # circle, circle -> ctraight
     begin = start.to(straight)
     encounter = straight.to(circle)
     avoid = circle.to(straight)
     end = straight.to(finish)
     # these are inputs of FSM
 
+    def __init__(self, env):
+        self.env = env
+        self.vel = 0.5
+
+    def colllision_detection(self):
+        # if np.linalg.norm(loc - env.x[robot,:]) <= certain value
+        
+    def decison_making(self):
+        env = self.env
+        for robot in range(env.n_agents):
+        # create different robot instances and store them in a list
+        # First i can press something to start the program, then the program will automatically decide when to do circular motion.
+        # begin: every robot- robot.begin
+        #   design the dir
+        #   return action matrix
+    
+    def get_actions_straight(self):
+        env = self.env
+        actions = np.zeros((env.n_agents, 2))
+        for robot in range(env.n_agents):
+            assigned_tasks = env.assignment_list[robot]
+            for task in assigned_tasks:
+                if not env.task_done[task]:
+                    loc = env.tasks[task, :]
+                    dir = (loc - env.x[robot, :]) / np.linalg.norm(loc - env.x[robot, :])
+                    actions[robot, :] = dir * self.vel
+                    break
+        return actions
+    def git_action_circular(self):
+        env = self.env
+        actions = np.zeros(((env.n_agents, 2)))
+        for robot in range(env.n_agents):
+            assigned_tasks = env.assignment_list[robot]
+            for task in assigned_tasks:
+                if not env.task_done[task]:
+                    loc = env.tasks[task, :]
+                    dir =
+
+        
 
 
--- simple_controller.py
+++ simple_controller.py
@@ -1,4 +1,4 @@
-mport numpy as np
+import numpy as np
 
 class SimpleController():
 
@@ -14,17 +14,10 @@ class SimpleController():
         for robot in range(env.n_agents):
             assigned_tasks = env.assignment_list[robot]
             for task in assigned_tasks:
-                if (not env.task_done[task]):
-                    if not True: #env.task_readiness[task] == 1:  # task is not ready
-                        loc = env.x[robot, :]  # don't move
-                        dir = np.zeros((2,))
-                        actions[robot, :] = dir * self.vel
-                        break
-                    else:
-                        loc = env.tasks[task, :]  # move towards location
-                        dir = (loc - env.x[robot, :]) / np.linalg.norm(loc - env.x[robot, :])
-                        actions[robot, :] = dir * self.vel
-                        break
-
+                if not env.task_done[task]:
+                    loc = env.tasks[task, :]
+                    dir = (loc - env.x[robot, :]) / np.linalg.norm(loc - env.x[robot, :])
+                    actions[robot, :] = dir * self.vel
+                    break
         return actions
 
