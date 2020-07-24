7070a84a545ca2442061492b2c4628be07480c8d
Tuo <willwong1016@gmail.com>
2020-07-24 18:36:14 -0400

    modified FSM controller

--- FSMcontroller.py
+++ FSMcontroller.py
@@ -3,24 +3,29 @@ import numpy as np
 
 
 
-class collisionAvoidance(StateMachine):
+class CollisionAvoidance(StateMachine):
     start = State('Start', initial=True)
     straight = State('Straight')
     circle = State('Circle')
     finish = State('Finish')
     # the lines above describe three possible state robots have: start -> straight, straight -> finish, straight ->
-    # circle, circle -> ctraight
+    # circle, circle -> straight
     begin = start.to(straight)
     encounter = straight.to(circle)
     avoid = circle.to(straight)
     end = straight.to(finish)
     # these are inputs of FSM
 
+    # robot = collisionAvoidance()
+    # robot.current_state
+
+
     def __init__(self, env):
         self.env = env
         self.vel = 0.5
 
-    def collision_detection(self):
+    def collision_detection(self, t_duration):
+        # globally detect distance between each robot at every time duration
         collision = False
         env = self.env
         actions = np.zeros((env.n_agents, 2))
@@ -29,55 +34,79 @@ class collisionAvoidance(StateMachine):
             for task in assigned_tasks:
                 if not env.task_done[task]:
                     loc = env.tasks[task,:]
-                    if np.linalg.norm(loc - env.x[robot,:]) < 2:
+                    if np.linalg.norm(loc - env.x[robot,:]) < 2: # set the recognition radius is 2
                         collision = True
         return collision
 
-    def decision_making(self, begin, encounter, avoid, end):
-        env = self.env
-        for robot in range(env.n_agents):
-            robot = collisionAvoidance()
-            if begin is True:
-                robot.begin
-            elif encounter is True:
-                robot.encounter
-            elif avoid is True:
-                robot.avoid
-            elif end is True:
-                robot.end
+    # def decision_making(self, begin, encounter, avoid, end):
+    #     env = self.env
+    #     for robot in range(env.n_agents):
+    #         robot = collisionAvoidance()
+    #         if begin is True:
+    #             robot.begin
+    #         elif encounter is True:
+    #             robot.encounter
+    #         elif avoid is True:
+    #             robot.avoid
+    #         elif end is True:
+    #             robot.end
 
+
+    def get_actions(self, t_duration):
+        env = self.env
+        actions = np.zeros((env.n_agents,2))
+        self.collision_detection(t_duration)
+        for robot in range (env.n_agents):
+            assigned_tasks = env.assignment_list[robot]
+            for task in assigned_tasks:
+                if not env.task_done[task]:
+                    loc = env.tasks[task,:]
+                    if
+
+
+
+
+# initialization
+    # robot class object
+    # append
+    #
 
         # create different robot instances and store them in a list
         # First i can press something to start the program, then the program will automatically decide when to do circular motion.
         # begin: every robot- robot.begin
         #   design the dir
         #   return action matrix
-    
-    def get_actions_straight(self):
-        env = self.env
-        actions = np.zeros((env.n_agents, 2))
-        for robot in range(env.n_agents):
-            assigned_tasks = env.assignment_list[robot]
-            for task in assigned_tasks:
-                if not env.task_done[task]:
-                    loc = env.tasks[task, :]
-                    dir = (loc - env.x[robot, :]) / np.linalg.norm(loc - env.x[robot, :])
-                    actions[robot, :] = dir * self.vel
-                    break
-        return actions
-
-
-    def git_action_circular(self):
-        env = self.env
-        actions = np.zeros(((env.n_agents, 2)))
-        for robot in range(env.n_agents):
-            assigned_tasks = env.assignment_list[robot]
-            for task in assigned_tasks:
-                if not env.task_done[task]:
-                    loc = env.tasks[task, :]
-                    dir =
-                    actions[robot, :] = dir * self.vel
-        return actions
+#
+#     def
+#
+#     def get_actions_straight(self):
+#         env = self.env
+#         actions = np.zeros((env.n_agents, 2))
+#         for robot in range(env.n_agents):
+#             assigned_tasks = env.assignment_list[robot]
+#             for task in assigned_tasks:
+#                 if not env.task_done[task]:
+#                     loc = env.tasks[task, :]
+#                     dir = (loc - env.x[robot, :]) / np.linalg.norm(loc - env.x[robot, :])
+#                     actions[robot, :] = dir * self.vel
+#                     break
+#         return actions
+#
+# # if np. <= 2
+#         # dir  =
+# #elif
+#    # dir = straight
+#     def git_action_circular(self,t):
+#         env = self.env
+#         actions = np.zeros((env.n_agents, 2))
+#         for robot in range(env.n_agents):
+#             assigned_tasks = env.assignment_list[robot]
+#             for task in assigned_tasks:
+#                 if not env.task_done[task]:
+#                     loc = env.tasks[task, :]
+#                     dir =
+#                     actions[robot, :] = dir * self.vel
+#         return actions
 
         
 
---
+++ simple_experiment_copy.py
@@ -0,0 +1,125 @@
+import numpy as np
+import argparse
+import scipy.io as sio
+from datetime import datetime
+import os
+import importlib
+import FSMcontroller
+
+def set_seed(seed_value):
+    if seed_value == None:
+        seed_val = np.random.randint(0, 1000)
+    else:
+        seed_val = seed_value
+    np.random.seed(seed_val)
+    return seed_val
+
+
+def get_args():
+    parser = argparse.ArgumentParser(description=None)
+    parser.add_argument('--planner', default='simple_planner', dest='planner', type=str,
+                        help="planner to use: can be simple_planner, from_file, etc.?")
+    parser.add_argument('--controller', default='simple_controller', dest='controller', type=str,
+                        help="controller to use: can be simple_controller, others to be implemented")
+    parser.add_argument('--seed', default=None, dest='seed_val', type=int,
+                        help="seed value for random number generator, int")
+    parser.add_argument('--filename', default=None, dest='filename_str', type=str,
+                        help="if importing from file, specify which file. Otherwise, choose the most recent matlab_out")
+    parser.add_argument('--params', default='dependency_test_params',dest='params_name',type=str,help='set the parameter file to be used')
+    return parser.parse_args()
+
+
+def get_list_from_file(filename='matlab_out'):
+    d = sio.loadmat(filename)
+    list_raw = d['assignment_list']
+    print(list_raw)
+    a_list = []
+    for item in list_raw[0]:
+        a_list.append(item[0])
+    return a_list
+
+
+if __name__ == "__main__":
+    args = get_args()
+    seed_val = set_seed(args.seed_val)
+
+    import simple_env
+    import simple_planner
+    import simple_controller
+    #import centralized_hungarian_nx
+
+    # create environment ---------------------------------------------------------
+    nsteps = 1000
+    params = importlib.import_module(args.params_name)
+    env = simple_env.SimpleEnv(params)
+    robot_diameter = env.robot_diameter
+
+    # process planner argument------------------------------------------------------
+    if args.planner == 'simple_planner':
+        planner = simple_planner.SimplePlanner(env)
+        assignment_list = planner.plan()
+    elif args.planner == 'centralized_planner':
+        raise Exception('centralized planner not yet implemented.')
+        assignment_list = planner.plan()
+
+    elif args.planner == 'from_file':
+        if args.filename_str != None:
+            assignment_list = get_list_from_file(args.filename_str)
+        else:
+            assignment_list = get_list_from_file()
+    else:
+        raise Exception('invalid argument for --planner')
+
+    # process controller argument-------------------------------------------------
+    if args.controller == 'simple_controller':
+        controller = simple_controller.SimpleController(env)
+    else:
+        raise Exception('invalid argument for --controller')
+
+    env.assignment_list = assignment_list
+    env.build_assignment_matrix()
+
+    # run controller---------------------------------------------------------------
+    t = 0
+    done = False
+    current_tasks = np.zeros((env.n_agents,))
+
+    while t < nsteps and not done:
+        # calculate controls
+
+        actions = controller.get_actions(t)
+
+        # apply controls
+        newstate, completion = env.step(actions)
+
+        done = completion.all()
+        t += 1
+
+    env.plot()
+
+    # export data
+
+    today = datetime.now()
+
+    dir_string = "data/" + today.strftime('%Y%m%d')
+    try:
+        os.mkdir(dir_string)
+    except FileExistsError:
+        pass
+    np.savez(dir_string + "/data_" + today.strftime("%H%M"), seed=seed_val, assignment_list=assignment_list,
+             t=t / env.dt, allow_pickle=True)
+
+    # generate travel time for matlab
+    ll = env.n_agents+env.n_tasks
+    ltasks = np.concatenate((env.state_history[0],env.tasks),axis=0)
+    tt = np.zeros((ll ** 2, 1))
+    for ii in range(ll):
+        for jj in range(ll):
+            tt[ii * ll + jj] = np.linalg.norm(ltasks[ii, :] - ltasks[jj, :])
+
+    ldurations = np.concatenate((np.zeros((env.n_agents, 1)), env.durations))
+
+    ldependency = np.zeros((ll,ll))
+    ldependency[env.n_agents:,env.n_agents:] = env.task_dependency_matrix
+    sio.savemat("matlab_inputs", {"na": env.n_agents, "nk": env.n_tasks+env.n_agents, "dependency": ldependency,
+                                  "cost_vector": ldurations, "travel_time": tt})
