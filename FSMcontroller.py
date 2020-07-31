4a2ca71705429bf4bfc4489cd8ee660d47b60bb5
Tuo <willwong1016@gmail.com>
2020-07-31 16:11:04 -0400

    Modified

--- FSMcontroller.py
+++ FSMcontroller.py
@@ -36,8 +36,8 @@ class CollisionAvoidance(StateMachine):
                 for task1 in assigned_tasks2:
                     if not env.task_done[task]:
                         loc = env.tasks[task, :]
-                    if not env.task_done[task2]:
-                        loc2 = env.tasks[task2, :]
+                    if not env.task_done[task1]:
+                        loc2 = env.tasks[task1, :]
                     if np.linalg.norm(loc - loc2) < 2:  # set the recognition radius is 2
                         collision = True
         return collision
@@ -47,13 +47,15 @@ class CollisionAvoidance(StateMachine):
         env = self.env
         actions = np.zeros((env.n_agents, 2))
         collision = self.collision_detection()
-        for robot, robot1 in range(env.n_agents), range(env.n_agents):
-            assigned_tasks = env.assignment_list[robot]
-            assigned_tasks1 = env.assignment_list[robot1]
-            for task, task1 in assigned_tasks, assigned_tasks1:
-                if not (env.task_done[task] and env.task_done[task1]):
-                    loc = env.tasks[task, :]
-                    loc1 = env.tasks[task1, :]
+        for robot in range(env.n_agents):
+            for robot1 in range(env.n_agents):
+                assigned_tasks = env.assignment_list[robot]
+                assigned_tasks1 = env.assignment_list[robot1]
+            for task in assigned_tasks:
+                for task1 in assigned_tasks1:
+                    if not (env.task_done[task] and env.task_done[task1]):
+                        loc = env.tasks[task, :]
+                        loc1 = env.tasks[task1, :]
                     if collision is False:
                         dir = (loc - env.x[robot, :]) / np.linalg.norm(loc - env.x[robot, :])
                     else:
@@ -64,8 +66,8 @@ class CollisionAvoidance(StateMachine):
             return actions
 
     def make_trajectory(self, loc, loc1):
-        x = (loc[:,0] + loc1[:,0]) / 2
-        y = (loc[0,:] + loc1[:,0]) / 2
-        dir = np.cross((loc - [x,y]) / np.linalg.norm(loc - [x,y]), [0,0,1])
+        x = (loc[0] + loc1[0]) / 2
+        y = (loc[1] + loc1[1]) / 2
+        dir = np.cross((loc - [x,y]) / np.linalg.norm(loc - [x,y]), [0,0,1])[:2]
         return dir
 
