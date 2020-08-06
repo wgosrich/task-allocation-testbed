f7d01feeebecfe14ffd1991b0449a4f30785b6fd
tuo <willwong1016.gmail.com>
2020-08-06 12:37:42 -0400

    modified

--- centralized_planner.py
+++ centralized_planner.py
@@ -10,21 +10,23 @@ class Planner:
     def __init__(self,env):
         self.env = env
     def agent_state_reset(self, col_ind, n_agents, tasks):
-        new_agent_state = [[] for _ in range(n_agents)]
+        new_agent_state = np.zeros((n_agents,2))
         for i in range(len(col_ind)):
             new_agent_state[i] = tasks[col_ind[i]]
         return new_agent_state
 
-    def create_matrix (self, distanceMatrix, n_agents, task_list,agent_state, task_state, ):
-
+    def create_matrix (self, distanceMatrix, n_agents, task_list,agent_state, task_state ):
+        distanceMatrix = np.zeros((n_agents, task_state.shape[0]))
+        task_number = task_state.shape[0]
         n_tasks = len(task_state)
         for i in range(n_agents):
-            for j in range(len(task_list)):
-                distanceMatrix[i][j] = (agent_state[i][0] - task_state[j][0]) ** 2 + (agent_state[i][1] - task_state[j][1]) ** 2
+            for j in range(task_number):
+                distanceMatrix[i,j] = np.linalg.norm(agent_state[i,:]-task_state[j,:])**2
+                #distanceMatrix[i][j] = (agent_state[i][0] - task_state[j][0]) ** 2 + (agent_state[i][1] - task_state[j][1]) ** 2
         if n_agents < len(task_list):
-            dummy_distanceM = np.zeros((len(task_list) - n_agents, len(task_list)))
+            dummy_distanceM = np.zeros((len(task_list) - n_agents, task_number))
             for i4 in range(len(task_list) - n_agents):
-                for j in range(len(task_list)):
+                for j in range(task_number):
                     dummy_distanceM[i4][j] = np.max(distanceMatrix)
             new_distanceM = np.r_[distanceMatrix, dummy_distanceM]
         else:
@@ -40,8 +42,8 @@ class Planner:
         new_col_ind = np.zeros(((math.ceil(n_tasks / n_agents)), n_agents))
         for i6, i12 in zip(row_ind, range(len(col_ind))):
             new_col_ind[i2][i6] = task_list[col_ind[i12]]
-        task_list = [task_list[i] for i in range(len(task_list)) if (i not in col_ind)]
-        task_state = [self.env.tasks[task_list[i]] for i in range(len(task_list))]
+        task_list = np.array([task_list[i] for i in range(len(task_list)) if (i not in col_ind)])
+        task_state = np.array([self.env.tasks[task_list[i]] for i in range(len(task_list))])
         col_indList[i2] = new_col_ind[i2]
         return new_col_ind, col_indList, task_state
 
@@ -53,11 +55,11 @@ class Planner:
         task_state = self.env.tasks
         task_list = np.arange(n_tasks)
         for i2 in range(math.ceil(n_tasks / n_agents)):
-            distanceMatrix = np.zeros((n_agents, len(task_list)))
+            distanceMatrix = np.zeros((n_agents, task_state.shape[0]))
             row_ind, col_ind, n_tasks = self.create_matrix(distanceMatrix,n_agents,task_list,agent_state,task_state)
             new_col_ind, col_indList, task_state= self.retore_position(row_ind, col_ind, task_list, i2, n_tasks, n_agents)
             reset_col_ind = new_col_ind.astype(int)
-            agent_state = self.agent_state_reset(reset_col_ind[i2],n_agents,one_to_one_params.tasks)
+            agent_state = self.agent_state_reset(reset_col_ind[i2],n_agents,self.env.tasks)
         assignment_list = [[] for _ in range(n_agents)]
         for i5 in range(math.ceil(n_tasks2 / n_agents)):
             for i7 in range(n_agents):
@@ -65,4 +67,3 @@ class Planner:
         A = np.array(assignment_list)
         A = A.astype(int)
         return A
-
