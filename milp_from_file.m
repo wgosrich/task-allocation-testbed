e030bf53e2516e48e471c302642223e74d85a26f
wgosrich <wgosrich@gmail.com>
2020-08-06 17:17:19 -0400

    add timing to milp

--- milp_from_file.m
+++ milp_from_file.m
@@ -1,8 +1,9 @@
 %% import from file
 load('matlab_inputs','-mat')
-
+tic
 [x,A,Aeq,b,assignment_list] = milp_planner(na,nk,dependency,cost_vector,travel_time);
 %travel_time
+plan_time = toc;
 
 %save assignment list
 save('matlab_out.mat')
